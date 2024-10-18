8f0589cbaca47ddb2aabcebe3b3c7d3038ee887b
hriad20 <hriad20@yahoo.com>
2024-10-18 18:58:30 +0300

    formatted the file to be cleaner and indented

--- src/LuckySeven.java
+++ src/LuckySeven.java
@@ -3,73 +3,74 @@ import java.util.Scanner;
 
 public class LuckySeven {
     public static void main(String[] args) throws Exception {
-        //Initiate the methods
+        // Initiate the methods
         Random rand = new Random();
-        Scanner scan = new Scanner(System.in);       
-        //initilized the variables
+        Scanner scan = new Scanner(System.in);
+        // initilized the variables
         int number1;
         int number2;
         int number3;
         int balance = 5;
         int choiceValue;
 
-        //Making the while loop and this is containing the actual program of the lucky 7
+        // Making the while loop and this is containing the actual program of the lucky
+        // 7
         while (balance > 0) {
-            number1 = rand.nextInt(10)+1;
-            number2 = rand.nextInt(10)+1;
-            number3 = rand.nextInt(10)+1;
+            number1 = rand.nextInt(10) + 1;
+            number2 = rand.nextInt(10) + 1;
+            number3 = rand.nextInt(10) + 1;
             System.out.println("Your balance is: " + balance + " how much you wish to play with: ");
             choiceValue = scan.nextInt();
             // this will check and run the code according to the player choice
             switch (choiceValue) {
                 case 1:
-                balance = balance - 1;
-                System.out.println("You got: " + number1);
+                    balance = balance - 1;
+                    System.out.println("You got: " + number1);
                     if (number1 == 7) {
                         System.out.println("You won you got a seven, 3$ will be added to your balance!");
                         balance = balance + 3;
-                    }else{
+                    } else {
                         System.out.println("Sorry you lost, better luck next time!");
                     }
                     break;
                 case 2:
-                if (balance >= 2) {
-                balance = balance - 2;
-                System.out.println("You got: " + number1 + " / "+ number2);
-                if (number1 == 7 && number2 == 7) {
-                    System.out.println("You won you got 2 Seven, 5$ will be added to your balance!");
-                    balance = balance + 5;
-                }else{
-                    System.out.println("Sorry you lost, better luck next time!");
-                }
-            }else{
-                System.out.println("sorry your balance is "+ balance +" less than 2 try another value");
-            }
+                    if (balance >= 2) {
+                        balance = balance - 2;
+                        System.out.println("You got: " + number1 + " / " + number2);
+                        if (number1 == 7 && number2 == 7) {
+                            System.out.println("You won you got 2 Seven, 5$ will be added to your balance!");
+                            balance = balance + 5;
+                        } else {
+                            System.out.println("Sorry you lost, better luck next time!");
+                        }
+                    } else {
+                        System.out.println("sorry your balance is " + balance + " less than 2 try another value");
+                    }
                     break;
                 case 3:
-                if (balance >= 3) {
-                balance = balance - 3;
-                System.out.println("You got: " + number1 + " / " + number2 + " / " + number3);
-                if (number1 == 7 && number2 == 7 && number3 == 7) {
-                    System.out.println("You won you got 3 Sevens, 10$ will be added to your balance!");
-                    balance = balance + 10;
-                }else{
-                    System.out.println("Sorry you lost, better luck next time!");
-                }
-            }else{
-                System.out.println("sorry your balance is "+ balance+" wich less than 3 try another value");
-            }
+                    if (balance >= 3) {
+                        balance = balance - 3;
+                        System.out.println("You got: " + number1 + " / " + number2 + " / " + number3);
+                        if (number1 == 7 && number2 == 7 && number3 == 7) {
+                            System.out.println("You won you got 3 Sevens, 10$ will be added to your balance!");
+                            balance = balance + 10;
+                        } else {
+                            System.out.println("Sorry you lost, better luck next time!");
+                        }
+                    } else {
+                        System.out.println("sorry your balance is " + balance + " wich less than 3 try another value");
+                    }
                     break;
-            
+
                 default:
                     break;
             }
 
         }
-        
+
         scan.close();
 
         System.out.println("Game over, unfortunately you lost all your money!");
-        
+
     }
 }
